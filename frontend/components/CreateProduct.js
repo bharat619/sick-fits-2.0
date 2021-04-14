7334832c60f62232dcbb63738c3ede1aa321ee39
bharat619 <bharat.h@kiprosh.com>
2021-04-15 00:07:06 +0530

    Add show page

---
+++ frontend/components/CreateProduct.js
@@ -0,0 +1,116 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import Router from 'next/router';
+import useForm from '../lib/useForm';
+import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # which variables are getting passed in and wht types are they
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        status: "AVAILABLE"
+        price: $price
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      description
+      price
+      name
+    }
+  }
+`;
+
+export default function CreateProduct() {
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    name: 'Wohoo',
+    image: '',
+    price: 6543456,
+    description: 'Woooooooo',
+  });
+
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
+    }
+  );
+
+  return (
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        // submit the input to backend
+        const res = await createProduct();
+        console.log(res);
+        clearForm();
+        // Go to that product's page!
+        Router.push({
+          pathname: `/products/${res.data.createProduct.id}`,
+        });
+      }}
+    >
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
+        <label htmlFor="name">
+          Name
+          <input
+            required
+            type="text"
+            name="name"
+            id="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            name="image"
+            id="image"
+            onChange={handleChange}
+          />
+        </label>
+
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            name="price"
+            id="price"
+            placeholder="Name"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label htmlFor="description">
+          Description
+          <textarea
+            name="description"
+            id="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Add Product</button>
+      </fieldset>
+    </Form>
+  );
+}
---
+++ frontend/lib/useForm.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // create a stte object for our inputs
+  const [inputs, setInputs] = useState(initial);
+
+  function handleChange(e) {
+    let { value, name, type } = e.target;
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+
+    if (type === 'file') {
+      // equivalent to value = e.target.files[0]
+      [value] = e.target.files;
+    }
+
+    setInputs({
+      // copy the existing state
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+
+    setInputs(blankState);
+  }
+
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
